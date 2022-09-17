4afc8bcbd900dfd36d386d604c32a3a232f2664c
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-09-17 17:50:24 +0900

    7-1 モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_17_044608) do
+ActiveRecord::Schema.define(version: 2022_09_17_083005) do
+
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.text "comment", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "post_id", null: false
